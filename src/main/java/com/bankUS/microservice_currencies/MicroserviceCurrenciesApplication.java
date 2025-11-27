0622b71d0dfac0218303596009ca7cf91a481910
alvvigsua <alvvigsua@alum.us.es>
2025-11-27 11:32:56 +0100

    Innitial commit

---
+++ src/main/java/com/bankUS/microservice_currencies/MicroserviceCurrenciesApplication.java
@@ -0,0 +1,13 @@
+package com.bankUS.microservice_currencies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceCurrenciesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceCurrenciesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bankUS/microservice_currencies/MicroserviceCurrenciesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bankUS.microservice_currencies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceCurrenciesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
